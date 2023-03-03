a0e7768e89e78f7d980687c8996e1eb2830d60ea
Sjoerd Brattinga <sjoerd-j-b@hotmail.com>
2023-03-03 17:42:00 +0100

    Added apiBase and apiKey in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    runtimeConfig: {
+        // The private keys which are only available server-side
+        //apiKey: '6d3a42a3-6d93-4f98-838d-bcc0ab2307fd'
 
+        // Keys within public are also exposed client-side
+        public: {
+          apiBase: 'https://api.dekamarkt.nl/v1',
+          apiKey: '6d3a42a3-6d93-4f98-838d-bcc0ab2307fd'
+        }
+      }
 })
